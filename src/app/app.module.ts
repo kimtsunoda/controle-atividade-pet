942618f2a3bb63a13a804ae64c52db370d7d7f2f
Kim Tsunoda <kim.tsunoda@gmail.com>
2023-09-15 21:36:35 -0300

    Atividade 07 - Criação do projeto e componentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
+import { MenuComponent } from './menu/menu.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BemVindoComponent,
+    MenuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
