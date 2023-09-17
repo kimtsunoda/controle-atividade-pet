e8b7af820dd26364f42d530f93eaab27cd158424
Kim Tsunoda <kim.tsunoda@gmail.com>
2023-09-17 01:16:37 -0300

    Criação do layout com componentes e uso do conceito de data-binding

--- README.md
+++ README.md
@@ -13,7 +13,7 @@
 ## Checklist
 
 - [x] Criar o repositório no GitHub com a estrutura do Gitflow, ou seja, branches main e develop.
-- [ ] Usar componentes de algum framework CSS (Bootstrap, Materialize ou outro)
+- [x] Usar componentes de algum framework CSS (Bootstrap, Materialize ou outro)
 - [ ] Apresentar as telas com layout responsivo usando ou não algum framework CSS.
 - [ ] Construir páginas web com o conceito de componentes.
 - [ ] Criar o layout da aplicação com componentes, ou seja, o cabeçalho e rodapé precisam ser componentes.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BemVindoComponent } from './bem-vindo/bem-vindo.component';
 import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BemVindoComponent,
-    MenuComponent
+    MenuComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
